18fede2e66dcf5f281a7816a6db63bd1e004197a
Jin Budelmann <gbudelmann@gmail.com>
2012-02-20 16:27:57 +1300

    Fixed iOS4 stretchable image issue Added iOS4 and ARC note in README

--- Classes/TabBarController.m
+++ Classes/TabBarController.m
@@ -13,7 +13,12 @@
 - (void) viewDidLoad {
     self.tabBar.backgroundImage = [UIImage imageNamed:@"bg"];
     
-    self.tabBar.selectionIndicatorImage = [[UIImage imageNamed:@"bg-selected"] resizableImageWithCapInsets:UIEdgeInsetsMake(6, 5, 5, 5)];
+    UIImage* selected = [UIImage imageNamed:@"bg-selected"];
+    if ([selected respondsToSelector:@selector(resizableImageWithCapInsets:)]) {
+        self.tabBar.selectionIndicatorImage = [selected resizableImageWithCapInsets:UIEdgeInsetsMake(6, 5, 5, 5)];
+    } else {
+        self.tabBar.selectionIndicatorImage = [selected stretchableImageWithLeftCapWidth:5 topCapHeight:6];
+    }
 }
 
 @end
